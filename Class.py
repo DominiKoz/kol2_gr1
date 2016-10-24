afe8c352c9317895029333701f5db629ed968e1e
DominiKoz <dominik.kozlowski@fis.agh.edu.pl>
2016-10-24 18:41:35 +0200

    initial commit

---
+++ Class.py
@@ -0,0 +1,11 @@
+class Class(object):
+	def __init__(self,name):
+		self.name = name
+		self.student_list = [Student("testname", "testsurname")]
+
+	def add_student(self, student, name, surename):
+		self.student_list.append(student)
+	
+	def add_score(self,student,mark):
+		self.student_list[]
+		
